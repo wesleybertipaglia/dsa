896fa3446ee6af2fbc925c7f122feeaf1126e9d6
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-03-09 14:12:34 -0300

    feat: implement a Stack

---
+++ learn/3-data-structures/1-linear/3-stack/Stack.py
@@ -0,0 +1,15 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+    
+    def push(self, item):
+        self.stack.append(item)
+    
+    def pop(self):
+        return self.stack.pop() if self.stack else None
+    
+    def peek(self):
+        return self.stack[-1] if self.stack else None
+    
+    def is_empty(self):
+        return not self.stack
